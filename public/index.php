11e388bd86278c6bfd5611f8ade6d6411873a5e4
Eko Wijaya <ekowijayawindows@gmail.com>
2021-01-03 14:56:30 +0700

    mk 5

--- public/index.php
+++ public/index.php
@@ -22,7 +22,7 @@ $pass_signature = true;
 
 
 // set LINE channel_access_token and channel_secret
-$channel_access_token = "3ZF2ZlYvbNJWluseUC/stGXUErGe9Hlyvfy69bgmKwCiIIG5S+vXS+MuNr4QpNSoGM03imcNaIy1JX4sNXQEhZ6r0UDZPeLdCcpjwlO5nchcUOswVNHYMRTDsVZWfrayDz4TEcPIobH9ePXFhpqYXwdB04t89/1O/w1cDnyilFU=";
+$channel_access_token = "jstYcaIgYqkFOHHTAKa8ewCUg9fa7yizwQVcbhzxGmnL5TNQXam6vWyxXHFrSyMwGM03imcNaIy1JX4sNXQEhZ6r0UDZPeLdCcpjwlO5ncjcAnvDULhyBUHWbHmp48tofwgwGLUboZ4/rY0+20+tQQdB04t89/1O/w1cDnyilFU=";
 $channel_secret = "f8d98388fa6f06ba4cd71c05e4efa630";
 
 
@@ -40,7 +40,7 @@ $app->setBasePath("/public");
 
 
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello Dunia");
+    $response->getBody()->write("Hello World!");
     return $response;
 });
 
@@ -75,62 +75,28 @@ $app->post('/webhook', function (Request $request, Response $response) use ($cha
         foreach ($data['events'] as $event) {
             if ($event['type'] == 'message') {
                 if ($event['message']['type'] == 'text') {
-                    switch (strtolower($event['message']['text'])) {
-                        case 'start':
-                            $text = "Hi, Nama saya Mark \n\nSaya adalah BOT yang akan membantu kamu untuk memesan \n\nKetik 'Menu' untuk memunculkan menu";
-                            $result = $bot->replyText($event['replyToken'], $text);
-                            break;
-                        case 'menu':
-                            $flexTemplate = file_get_contents("../flex_message.json"); // template flex message
-                            $result = $httpClient->post(LINEBot::DEFAULT_ENDPOINT_BASE . '/v2/bot/message/reply', [
-                                'replyToken' => $event['replyToken'],
-                                'messages'   => [
-                                    [
-                                        'type'     => 'flex',
-                                        'altText'  => 'Test Flex Message',
-                                        'contents' => json_decode($flexTemplate)
-                                    ]
-                                ],
-                            ]);
-                            break;
-                        case 'order':
-                            $text = "Terima Kasih \n\nPesanan anda sedang diproses";
-                            $result = $bot->replyText($event['replyToken'], $text);
-                            break;
-                        case 'order - soto ayam':
-                            $text = "Terima Kasih \n\nPesanan Soto Ayam anda sedang diproses";
-                            $result = $bot->replyText($event['replyToken'], $text);
-                            break;
-                        case 'order - bakso':
-                            $text = "Terima Kasih \n\nPesanan Bakso anda sedang diproses";
-                            $result = $bot->replyText($event['replyToken'], $text);
-                            break;
-                        case 'order - sate ayam':
-                            $text = "Terima Kasih \n\nPesanan Bakso anda sedang diproses";
-                            $result = $bot->replyText($event['replyToken'], $text);
-                            break;
-                        default:
-                            $text = "KEYWORD yang anda masukkan salah \n\nKetik START untuk menggunakan BOT";
-                            $result = $bot->replyText($event['replyToken'], $text);
-                            break;
-                    }
+                    // send same message as reply to user
+                    $result = $bot->replyText($event['replyToken'], $event['message']['text']);
+
+
+
+
+                    // or we can use replyMessage() instead to send reply message
+                    // $textMessageBuilder = new TextMessageBuilder($event['message']['text']);
+                    // $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
+
+
+
 
                     $response->getBody()->write(json_encode($result->getJSONDecodedBody()));
                     return $response
                         ->withHeader('Content-Type', 'application/json')
                         ->withStatus($result->getHTTPStatus());
-                } else {
-                    $text = "KEYWORD yang anda masukkan salah \n\nKetik START untuk menggunakan BOT";
-                    $result = $bot->replyText($event['replyToken'], $text);
                 }
             }
         }
-        return $response->withStatus(200, 'for Webhook!');
+        return $response->withStatus(200, 'for Webhook!'); //buat ngasih response 200 ke pas verify webhook
     }
     return $response->withStatus(400, 'No event sent!');
 });
-
-
-
-
 $app->run();
